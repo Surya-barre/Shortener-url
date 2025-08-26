341809f3e3c908686ffa082d6d106fbf49b10a4f
suryanarayana <barresuryanarayana18@gmail.com>
2025-08-26 12:37:06 +0530

    updateds

--- mern-url-shortener/backendpart/index.js
+++ mern-url-shortener/backendpart/index.js
@@ -11,12 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 // ✅ connect to MongoDB using .env variable
+
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("✅ MongoDB connected successfully"))
+  .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.use("/api", urlRoutes);
